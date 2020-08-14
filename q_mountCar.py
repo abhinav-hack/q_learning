510930632a0dd7bd11a5b157c3feeb5b540d0f32
abhihack <abhijeetbhavankar309@gmail.com>
2020-08-14 23:09:50 +0530

    commiting withmodified q_mountcar file

--- q_mountCar.py
+++ q_mountCar.py
@@ -2,28 +2,32 @@
 
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
-learning_rate = 0.1
-discount = 0.95
-episodes = 1000
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 10000
 
-show_every = 200
+SHOW_EVERY = 1000
 
 
 env = gym.make("MountainCar-v0")            # making enviorment
 #print("observation_space", env.observation_space)
 #print("action_space", env.action_space)
 
-discrete_os_size = [20]* len(env.observation_space.high)        # creating array of 20* len of observation 
+discrete_os_size = [40]* len(env.observation_space.high)        # creating array of 20* len of observation 
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/discrete_os_size   # creating discrete win
 
-epsilon = 0.5           # adding epsilon for randomness
+epsilon = 0.0          # adding epsilon for randomness
 start_epsilon = 1
-end_epsilon = episodes//2
+end_epsilon = EPISODES//2
 epsilon_decay_value = epsilon /(end_epsilon - start_epsilon)
 
+q_table = np.load("/root/Documents/reinforcement/q_table/0-q_table.npy")
+#q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_size+[env.action_space.n])) # qtable 20*20*3
 
-q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_size+[env.action_space.n])) # qtable 20*20*3
+ep_rewards = []
+aggr_ep_rewards = {"ep":[], "avg":[], "max":[], "min":[]}
 
 #print("win size", discrete_os_win_size)
 def get_discrete_state(state):      # Converting state to discrete states for input
@@ -31,9 +35,10 @@ def get_discrete_state(state):      # Converting state to discrete states for in
     return tuple(discrete_state.astype(int))
 
 
-for lap in range(episodes):     # running over episodes
+for lap in range(EPISODES):     # running over EPISODES
 
-    if lap % show_every == 0:       # for rendering every 
+    episod_reward = 0
+    if lap % SHOW_EVERY == 0:       # for rendering every 
         render = True
     else :
         render = False
@@ -49,29 +54,47 @@ for lap in range(episodes):     # running over episodes
 
         
         new_state, reward, done, _ = env.step(action)       # use that action
-        
+
+        episod_reward += reward
         if render :                 #immediatly render after action for total video
             env.render()  
 
-        if reward!=-1:                                      #  if reward is 0 that means flag is crossed
-            print(reward)
+        
         new_discrete_state = get_discrete_state(new_state)      #get discrete value of new state
         
                                       #render image
         if not done:                                        
             max_future_q = np.max(q_table[new_discrete_state])      # taking value of max q of new state
             current_q = q_table[discrete_state+(action, )]          # taking value of max q of previous state
-            new_q = (1- learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)     # eq for new q
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)     # eq for new q
             q_table[discrete_state +(action, )] = new_q             #updating new q for current state
         elif new_state[0] >= env.goal_position:                 # if new state is win
             q_table[discrete_state + (action,)] = 0             # update previous q with max value 0
-            print("winner",lap, q_table[discrete_state], reward)
+#            print("winner",lap, episod_reward)
         discrete_state = new_discrete_state
     
     # epsilon decay 
     if start_epsilon <= epsilon <= end_epsilon:
         epsilon -= epsilon_decay_value
 
+    ep_rewards.append(episod_reward)
+
+    if not lap % SHOW_EVERY:
+        average_reward = sum(ep_rewards[-SHOW_EVERY:])/ len(ep_rewards[-SHOW_EVERY:])
+        aggr_ep_rewards['ep'].append(lap)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+        aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
+        
+        print(f"Episode:{lap} avg:{average_reward} min:{min(ep_rewards[-SHOW_EVERY:])} max:{max(ep_rewards[-SHOW_EVERY:])}")
+
+    if lap % 200 ==0:
+        np.save(f"/root/Documents/reinforcement/q_table/{lap}-q_table.npy", q_table)
 
 env.close()
 
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label= 'avg')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label= 'min')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label= 'max')
+plt.legend(loc='best')
+plt.show()
